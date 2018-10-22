3cfcbb6e6f0c19afa37642e57e3943bda7c1b7dd
team006 <wachirachai.006@mail.kmutt.ac.th>
2018-10-22 19:50:15 +0700

    update css

--- src/main/java/com/softwareprocess/SubjectService.java
+++ src/main/java/com/softwareprocess/SubjectService.java
@@ -1,10 +1,5 @@
 package com.softwareprocess;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 public interface SubjectService {
 	Subject findOneSubject();
 	Subject findByName(String name);
